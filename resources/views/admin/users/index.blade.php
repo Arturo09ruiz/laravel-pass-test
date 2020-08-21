59ed2db8b4f520020275115dfedb15ada3e60ca8
Arturo <gregory09ruiz@gmail.com>
2020-08-21 19:12:32 -0400

    datatble spanish

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -54,6 +54,33 @@
     <script>
         $(document).ready(function() {
           $table =  $('#users').DataTable({
+            "language": {
+
+                "sProcessing":     "Procesando...",
+                "sLengthMenu":     "Mostrar _MENU_ registros",
+                "sZeroRecords":    "No se encontraron resultados",
+                "sEmptyTable":     "Ningún dato disponible en esta tabla",
+                "sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_",
+                "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0",
+                "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+                "sInfoPostFix":    "",
+                "sSearch":         "Buscar:",
+                "sUrl":            "",
+                "sInfoThousands":  ",",
+                "sLoadingRecords": "Cargando...",
+                "oPaginate": {
+                "sFirst":    "Primero",
+                "sLast":     "Último",
+                "sNext":     ">",
+                "sPrevious": "<"
+                },
+                "oAria": {
+                    "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+                    "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+                }
+
+                },
+
                 "serverSide": true,
                 "ajax" : "{{url('api/users')}}",
                 "columns":[
@@ -64,6 +91,7 @@
                     {data: 'created_at'},
                     {data: 'btn'},
                 ]
+
             });
         } );
 
