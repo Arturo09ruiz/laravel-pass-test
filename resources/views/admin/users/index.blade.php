b66823a35b91ef925d5be984ed4826813f1863ef
Arturo <gregory09ruiz@gmail.com>
2020-08-21 15:09:08 -0400

    navbar users created

---
+++ resources/views/admin/users/index.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="Prueba de Superacion">
+  <meta name="author" content="Arturo Ruiz">
+  <title>Usuarios | Prueba de Superacion | Arturo Ruiz</title>
+
+  <!-- Fonts -->
+  <link rel="stylesheet" href="{{ asset('admin/fonts/1.css') }}">
+  <!-- Icons -->
+  <link rel="stylesheet" href="{{ asset('admin/vendor/nucleo/nucleo.css') }}" type="text/css">
+  <link rel="stylesheet" href="{{ asset('admin/vendor/font-awesome/all.min.css') }}" type="text/css">
+  <!-- Page plugins -->
+  <!-- CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/css/select2.min.css" rel="stylesheet" />
+
+  <link rel="stylesheet" href="{{ asset('admin/css/1.css') }}" type="text/css">
+</head>
+
+<body>
+    @include('name')
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <script src="{{ asset('admin/vendor/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('admin/vendor/bootstrap_js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('admin/vendor/js-cookie/js.cookie.js') }}"></script>
+    <script src="{{ asset('admin/vendor/jquery.scrollbar/jquery.scrollbar.min.js') }}"></script>
+    <script src="{{ asset('admin/vendor/jquery-scroll-lock/jquery-scrollLock.min.js') }}"></script>
+    <!-- Optional JS -->
+    <script src="{{ asset('admin/vendor/chart.js/Chart.min.js') }}"></script>
+    <script src="{{ asset('admin/vendor/chart.js/Chart.extension.js') }}"></script>
+    <!-- Arturo JS -->
+    <script src="{{ asset('admin/js/1.js') }}"></script>
+    <script src="{{ asset('admin/vendor/datatables/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('admin/vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('admin/vendor/maskedinput/maskedinput.js') }}"></script>
+
+
+
+
+</body>
+</html>
