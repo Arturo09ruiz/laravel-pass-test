d533b11b42a99af0ebce8ca6bb151e1aeb418480
Arturo <gregory09ruiz@gmail.com>
2020-08-21 13:14:42 -0400

    users table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('status', ['Active', 'Disabled'])->default('Disabled');
             $table->rememberToken();
             $table->timestamps();
         });
